75b73855e0ac3b5af989596e4ed41e1ff4356965
Alvaro Cabrera <pateketrueke@gmail.com>
2013-09-04 01:33:56 -0500

    Improve camelize() helper settings

--- app/js/mohawk.js
+++ app/js/mohawk.js
@@ -24,7 +24,7 @@
 
   // nice handlers
   var camelize = function (str) {
-    return str.replace(/(_[a-z])/g, function ($1) { return $1.substr(1).toUpperCase(); });
+    return str.replace(/([._-][a-z])/g, function ($1) { return $1.substr(1).toUpperCase(); });
   };
 
 
