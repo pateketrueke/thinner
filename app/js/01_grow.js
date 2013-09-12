c96718303a0e9b65daec7f62a20b928afb9b0376
Alvaro Cabrera <aduran@expansion.com.mx>
2013-09-12 16:07:17 -0500

    Applies handler as thisArg, raises other methods also

--- app/js/01_grow.js
+++ app/js/01_grow.js
@@ -1,6 +1,8 @@
 
   // default hooks
   var grow = function (from, handler, is_plain) {
+    var key;
+
     if (! ('model' in handler)) {
       handler.model = proxy;
     }
@@ -11,11 +13,17 @@
     }
 
     handler.events = handler.events || {};
-    handler.events.error = raise(from, handler.events.error || undefined);
+    handler.events.error = raise.apply(handler, [from, handler.events.error || undefined]);
 
     // backward compatibility
     if (is_plain) {
       handler = handle(from.context, handler);
+    }
+
+
+    // error handling (?)
+    for (key in methods) {
+      handler[methods[key]] = raise.apply(handler, [from, handler[methods[key]]]);
     }
 
     return handler;
