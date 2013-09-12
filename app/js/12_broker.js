3e1a9c8ff0c06a432b7865c3fd8c420cc1c06d40
Alvaro Cabrera <aduran@expansion.com.mx>
2013-09-12 16:10:13 -0500

    All application classes are handled firstly; fix

--- app/js/12_broker.js
+++ app/js/12_broker.js
@@ -3,6 +3,16 @@
   var broker = function (app, name) {
     var klass = camelize(name);
 
+    // classes first
+    if (app.classes[klass]) {
+      if (! app.handlers[klass]) {
+        app.handlers[klass] = delegate(app, klass);
+      }
+
+      return app.handlers[klass];
+    }
+
+
     // backward compatibility
     if (app.handlers[name]) {
       if ('function' === typeof app.handlers[name]) {
@@ -17,14 +27,5 @@
       return app.handlers[name];
     }
 
-
-    if (! app.classes[klass]) {
-      throw new Error('<' + klass + '> undefined handler!');
-    }
-
-    if (! app.handlers[klass]) {
-      app.handlers[klass] = delegate(app, klass);
-    }
-
-    return app.handlers[klass];
+    throw new Error('<' + klass + '> undefined handler!');
   };
