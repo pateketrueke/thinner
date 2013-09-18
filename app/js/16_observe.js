d4c2952c343fca909c11d28e452ae9addb04d668
Alvaro Cabrera <pateketrueke@gmail.com>
2013-09-18 00:33:55 -0500

    Augment event arguments

--- app/js/16_observe.js
+++ app/js/16_observe.js
@@ -3,19 +3,22 @@
   var observe = function (app, evt) {
     // listen to every event from root
     root.on(evt + '.action', '.js-action', function (e) {
-      var key, action, handler, current, retval;
+      var key, action, handler, current, retval,
+          data, args, el;
 
       for (key in app.router.currentHandlerInfos) {
         current = app.router.currentHandlerInfos[key].handler;
 
         if ('object' === typeof current.actions) {
-          key = elem(e.currentTarget).attr('data-action');
+          el = elem(e.currentTarget);
+          data = el.data();
 
           for (action in current.actions) {
             handler = action.split('.')[0];
 
-            if (key === handler && action.lastIndexOf('.' + evt) > 0) {
-              retval = current[current.actions[action]].apply(current, arguments);
+            if (data.action === handler && action.lastIndexOf('.' + evt) > 0) {
+              args = [e, el, data].concat(slice.call(arguments, 1));
+              retval = current[current.actions[action]].apply(current, args);
             }
           }
         }
