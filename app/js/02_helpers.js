1e7dad7551a2846b5d5ebd93d67009b3690c21a2
Alvaro Cabrera <pateketrueke@gmail.com>
2013-09-22 18:03:09 -0500

    Tweak camelcase(); fix

--- app/js/02_helpers.js
+++ app/js/02_helpers.js
@@ -1,7 +1,7 @@
 
   // nice handlers
   var camelize = function (str) {
-    return str.replace(/([._-][a-z])/g, function ($1) { return $1.substr(1).toUpperCase(); });
+    return str.replace(/[._-][a-z]/g, function ($0) { return $0.substr(1).toUpperCase(); });
   };
 
 
